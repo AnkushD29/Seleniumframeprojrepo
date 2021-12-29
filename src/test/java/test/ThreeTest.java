bb7d17f570915ede57b42ee2dd381004cdf8c12b
AnkushD29 <82810874+AnkushD29@users.noreply.github.com>
2021-12-29 10:49:20 +0530

    Updated again by Krish

--- src/test/java/test/ThreeTest.java
+++ src/test/java/test/ThreeTest.java
@@ -11,8 +11,9 @@ public class ThreeTest extends Base {
 	public WebDriver driver;
 	@Test
 	public void testThree() throws Exception{
-		System.out.println("Krish has updated this code")
+		
 		System.out.println("Test Three");
+		System.out.println("Krish has updated this code");
 		driver=initializeDriver();
 		driver.get("http://www.tutorialsninja.com/demo/");
 		Thread.sleep(2000);
